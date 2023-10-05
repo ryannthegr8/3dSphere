c47fce46666180b4fd0e0dd265b20289dc85685c
ryannthegr8 <mmbaleryan@gmail.com>
2023-10-05 18:46:35 +0300

    sphere on the right

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
-// import { useState } from 'react'
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
-import Home from "./assets/components/Home";
+
+import * as THREE from "three";
+import { OrbitControls, Preload } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
-    <BrowserRouter>
-      <Home />
-    </BrowserRouter>
+    <>
+      <div className="container">
+        <div className="left">Goodness Heavens, just work mann</div>
+        <div id="canvas" className="right">
+          <Canvas
+            frameloop="demand"
+            shadows
+            gl={{ preserveDrawingBuffer: true }}
+          >
+            <ambientLight intensity={0.1} />
+            <directionalLight color="purple" position={[0, 0, 20]} />
+            <pointLight />
+            <mesh>
+              <sphereGeometry args={[2, 64, 64]} />
+              <meshPhysicalMaterial />
+            </mesh>
+            <OrbitControls enableZoom={false} />
+          </Canvas>
+        </div>
+      </div>
+    </>
   );
 }
 
