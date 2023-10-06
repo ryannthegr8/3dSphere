84c03b50708176725e1e4c3787f13e0e7ddc942e
ryannthegr8 <mmbaleryan@gmail.com>
2023-10-06 18:12:31 +0300

    Fully functional object

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
 import "./App.css";
-import React from "react";
-import * as THREE from "three";
-import { OrbitControls, Preload, Stars } from "@react-three/drei";
-import { Canvas, extend } from "@react-three/fiber";
+import React, { Suspense } from "react";
+import { OrbitControls, Preload, Stars, useTexture } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
 
-// extend({ anotherOne });
-
-// const anotherOne = () => {
-//   return (
-//     <mesh
-//       visible
-//       userData={{ hello: "world" }}
-//       position={[1, 2, 3]}
-//       rotation={[Math.PI / 2, 0, 0]}
-//     >
-//       <sphereGeometry args={[1, 16, 16]} />
-//       <meshStandardMaterial color="hotpink" transparent />
-//     </mesh>
-//   );
-// };
+function SpinningSphere() {
+  return (
+    <>
+      <OrbitControls enableZoom={false} />
+      <ambientLight intensity={0.1} />
+      <directionalLight color="purple" position={[0, 0, 20]} />
+      <pointLight />
+      <mesh>
+        <sphereGeometry args={[2, 64, 64]} />
+        <meshPhysicalMaterial />
+      </mesh>
+    </>
+  );
+}
 
 function App() {
   return (
@@ -26,23 +24,17 @@ function App() {
       <div className="container">
         {/* <div className="left">Goodness Heavens, just work mann</div> */}
         <div id="canvas" className="right">
-          <Canvas
-            frameloop="demand"
-            shadows
-            gl={{ preserveDrawingBuffer: true }}
-          >
-            <ambientLight intensity={0.1} />
-            <directionalLight color="purple" position={[0, 0, 20]} />
-            <pointLight />
-            <mesh>
-              <sphereGeometry args={[2, 64, 64]} />
-              <meshPhysicalMaterial />
-            </mesh>
-            {/* <anotherOne /> */}
-            <OrbitControls enableZoom={false} />
+          <Suspense fallback={null}>
+            <Canvas
+              frameloop="demand"
+              shadows
+              gl={{ preserveDrawingBuffer: true }}
+            >
+              <SpinningSphere />
 
-            <Preload all />
-          </Canvas>
+              <Preload all />
+            </Canvas>
+          </Suspense>
         </div>
       </div>
     </>
