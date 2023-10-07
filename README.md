dd9d852210a944fadb254fb6afafd0202545e286
ryannthegr8 <mmbaleryan@gmail.com>
2023-10-07 13:43:48 +0300

    Sphere with background

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # 3D Sphere
 
-Has concepts of a spinning sphere
+Has concepts of a spinning sphere in a 3D environment
+
+## How to start the project
+
+Once you have already cloned the project write the command below;
+
+            npm install && npm run dev
--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import "./App.css";
 import React, { Suspense } from "react";
-import { OrbitControls, Preload, Stars, useTexture } from "@react-three/drei";
+import {
+  CameraShake,
+  Cloud,
+  OrbitControls,
+  Preload,
+  Sky,
+  SpotLight,
+  Stars,
+  useTexture,
+} from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { Camera } from "three";
 
 function SpinningSphere() {
   return (
     <>
-      <OrbitControls enableZoom={false} />
+      <OrbitControls enableZoom={false} autoRotate autoRotateSpeed={1} />
       <ambientLight intensity={0.1} />
       <directionalLight color="purple" position={[0, 0, 20]} />
       <pointLight />
+      <Stars />
       <mesh>
         <sphereGeometry args={[2, 64, 64]} />
         <meshPhysicalMaterial />
